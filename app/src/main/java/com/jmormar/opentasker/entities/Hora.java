f9d0abe00ee129b3b05f11e011763091394f4815
jmormar2412 <jmormar2412@g.educaand.es>
2024-03-05 14:30:10 +0100

    Desarrollo de clase hora

--- app/src/main/java/com/jmormar/opentasker/entities/Hora.java
+++ app/src/main/java/com/jmormar/opentasker/entities/Hora.java
@@ -1,10 +1,15 @@
 package com.jmormar.opentasker.entities;
 
+import java.time.LocalDateTime;
+import java.time.Period;
+
 /**
  * <h1>Hora</h1>
  * <p>Las horas son segmentos de una duración determinada dispuestos en un horario. Pueden durar un tiempo definido por los atributos "Hora de inicio" y "Hora final".</p>
  * <p>El atributo Categoría determinará a qué asignatura pertenece. (Véase la clase Categoría para más información).</p>
  */
 public class Hora {
-
+    //Bua chaval vamos a utilizar la primera semana del mundo cíclicamente tio
+    private LocalDateTime fechayTiempoInicio;
+    private Period totalTiempo;
 }
