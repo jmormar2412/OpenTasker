e95ef7a274caeca81c83045df985642209b042c4
jmormar2412 <jmormar2412@g.educaand.es>
2024-03-05 14:13:13 +0100

    Se me había olvidado poner la descripción del horario tio

--- app/src/main/java/com/jmormar/opentasker/entities/Horario.java
+++ app/src/main/java/com/jmormar/opentasker/entities/Horario.java
@@ -1,4 +1,13 @@
 package com.jmormar.opentasker.entities;
 
+import java.util.ArrayList;
+
+/**
+ * <h1>Horario</h1>
+ * <p>El horario va a tener las horas que hay a lo largo de la semana y las va a mostrar en una línea temporal con los días y todos (porqui)</p>
+ */
 public class Horario {
+    //Decidir la forma de almacenar las horas, si directamente aquí o en arrays de días (me estoy muriendo ahora mismo)
+    private ArrayList<Hora> horas;
+    int idAgenda;
 }
