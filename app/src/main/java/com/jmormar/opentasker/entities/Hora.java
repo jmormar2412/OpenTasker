ff3deaa7810a172ab745e2b01fd7398b1bff6087
jmormar2412 <jmormar2412@g.educaand.es>
2024-03-08 14:25:35 +0100

    Añadido lombok

--- app/src/main/java/com/jmormar/opentasker/entities/Hora.java
+++ app/src/main/java/com/jmormar/opentasker/entities/Hora.java
@@ -3,13 +3,18 @@ package com.jmormar.opentasker.entities;
 import java.time.LocalDateTime;
 import java.time.Period;
 
+import lombok.Data;
+
 /**
  * <h1>Hora</h1>
  * <p>Las horas son segmentos de una duración determinada dispuestos en un horario. Pueden durar un tiempo definido por los atributos "Hora de inicio" y "Hora final".</p>
  * <p>El atributo Categoría determinará a qué asignatura pertenece. (Véase la clase Categoría para más información).</p>
  */
+@Data
 public class Hora {
     //Bua chaval vamos a utilizar la primera semana del mundo cíclicamente tio
+    private int idHora;
     private LocalDateTime fechayTiempoInicio;
     private Period totalTiempo;
+    private int idHorario;
 }
--- app/src/main/java/com/jmormar/opentasker/entities/Horario.java
+++ app/src/main/java/com/jmormar/opentasker/entities/Horario.java
@@ -2,12 +2,16 @@ package com.jmormar.opentasker.entities;
 
 import java.util.ArrayList;
 
+import lombok.Data;
+
 /**
  * <h1>Horario</h1>
  * <p>El horario va a tener las horas que hay a lo largo de la semana y las va a mostrar en una línea temporal con los días y todos (porqui)</p>
  */
+@Data
 public class Horario {
     //Decidir la forma de almacenar las horas, si directamente aquí o en arrays de días (me estoy muriendo ahora mismo)
+    private int idHorario;
     private ArrayList<Hora> horas;
-    int idAgenda;
+    private int idAgenda;
 }
