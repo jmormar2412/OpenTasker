37c03964a3652e2286007d65783b66b37e51af82
jmormar2412 <jmormar2412@g.educaand.es>
2024-03-13 12:29:10 +0100

    Añadido Data

--- app/src/main/java/com/jmormar/opentasker/entities/Tiempo.java
+++ app/src/main/java/com/jmormar/opentasker/entities/Tiempo.java
@@ -2,15 +2,17 @@ package com.jmormar.opentasker.entities;
 
 import java.time.Period;
 
+import lombok.Data;
+
 /**
  * <h1>Tiempo</h1>
  * <p>Los tiempos del pomodoro serán representados por esta clase. Pueden ser tanto tiempos normales como descansos.</p>
  */
+@Data
 public class Tiempo {
+    private int idTiempo;
     private String nombre;
-    //Como se almacena esto en la base de datos tio
     private Period tiempo;
-    //No permitir poner nombre en rest (o sí no se)
     private boolean rest;
     private int idPomodoro;
 }
