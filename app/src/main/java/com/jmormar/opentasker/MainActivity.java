225dfc1b8b2bd4def49b29aab262ab65c67aee65
jmormar2412 <jmormar2412@g.educaand.es>
2024-03-15 14:15:30 +0100

    Vamos a empezar no

--- app/src/main/java/com/jmormar/opentasker/MainActivity.java
+++ app/src/main/java/com/jmormar/opentasker/MainActivity.java
@@ -1,14 +1,8 @@
 package com.jmormar.opentasker;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.jmormar.opentasker.entities.Agenda;
-import com.jmormar.opentasker.util.DBHelper;
-
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,20 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView label = findViewById(R.id.displayLabel);
 
-        DBHelper helper = DBHelper.getInstance(this);
 
-        Agenda pp = new Agenda();
-        pp.setNombre("Test");
-        pp.setFechaInicio(new Date(454545465));
-        pp.setFechaFinal( new Date(460000000));
-
-        if(helper.insertarAgenda(pp)){
-            label.setText("Todo ha ido bien");
-            return;
-        }
-        label.setText("Jey ror");
 
     }
 }
