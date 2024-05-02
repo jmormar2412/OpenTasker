9281cb6e956ea037c203708f798919c73b5388a7
jmormar2412 <117973709+jmormar2412@users.noreply.github.com>
2024-05-02 13:45:42 +0200

    Implementación de Swipe y click

--- app/src/main/java/com/jmormar/opentasker/adapters/EventoAdapter.java
+++ app/src/main/java/com/jmormar/opentasker/adapters/EventoAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.jmormar.opentasker.R;
@@ -21,10 +22,15 @@ import java.util.List;
 public class EventoAdapter extends RecyclerView.Adapter<EventoAdapter.EventoViewHolder> {
     private List<Evento> eventos;
     private DBHelper helper;
+    private OnEventoClickListener mListener;
+    private boolean showHechos;
 
-    public EventoAdapter(Context context, List<Evento> eventos) {
+
+
+    public EventoAdapter(Context context, List<Evento> eventos, boolean showHechos) {
         this.eventos = eventos;
         helper = DBHelper.getInstance(context);
+        this.showHechos = showHechos;
     }
 
     @NonNull
@@ -45,6 +51,12 @@ public class EventoAdapter extends RecyclerView.Adapter<EventoAdapter.EventoView
         return eventos.size();
     }
 
+    public void setOnEventoClickListener(OnEventoClickListener mListener) {
+        this.mListener = mListener;
+    }
+
+
+
     class EventoViewHolder extends RecyclerView.ViewHolder {
         TextView nombre;
         TextView fecha;
@@ -57,16 +69,35 @@ public class EventoAdapter extends RecyclerView.Adapter<EventoAdapter.EventoView
             fecha = itemView.findViewById(R.id.evli_fecha);
             tipo = itemView.findViewById(R.id.evli_tipo);
             categoria = itemView.findViewById(R.id.evli_categoria);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if(position!=RecyclerView.NO_POSITION && mListener!=null) mListener.onEventoClick(position);
+                }
+            });
         }
 
         void bind(Evento evento) {
-            nombre.setText(evento.getNombre());
-            SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
-            fecha.setText(evento.getFecha() != null ? dateFormatter.format(evento.getFecha()) : "");
-            Tipo tp = helper.getTipo(evento.getIdTipo());
-            tipo.setText(tp.getNombre());
-            Categoria cat = helper.getCategoria(evento.getIdCategoria());
-            categoria.setText(cat.getNombre());
+            if (showHechos || !evento.isHecho()) {
+                nombre.setText(evento.getNombre());
+                SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+                fecha.setText(evento.getFecha() != null ? dateFormatter.format(evento.getFecha()) : "");
+                Tipo tp = helper.getTipo(evento.getIdTipo());
+                tipo.setText(tp.getNombre());
+                Categoria cat = helper.getCategoria(evento.getIdCategoria());
+                categoria.setText(cat.getNombre());
+                itemView.setVisibility(View.VISIBLE); // Make sure the itemView is visible
+                itemView.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)); // Reset the layout params to default
+            } else {
+                itemView.setVisibility(View.GONE);
+                itemView.setLayoutParams(new RecyclerView.LayoutParams(0, 0));
+            }
         }
+    }
+
+    public interface OnEventoClickListener {
+        void onEventoClick(int position);
     }
 }
