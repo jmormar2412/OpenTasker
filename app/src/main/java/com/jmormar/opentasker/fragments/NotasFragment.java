e945ee16e8324618376186f2a6791fbdaf1028fd
jmormar2412 <117973709+jmormar2412@users.noreply.github.com>
2024-05-07 21:25:16 +0200

    Creación y definición del fragmento de notas.

--- app/src/main/java/com/jmormar/opentasker/fragments/NotasFragment.java
+++ app/src/main/java/com/jmormar/opentasker/fragments/NotasFragment.java
@@ -1,43 +1,42 @@
 package com.jmormar.opentasker.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.jmormar.opentasker.R;
+import com.jmormar.opentasker.adapters.NotaAdapter;
+import com.jmormar.opentasker.models.Nota;
+import com.jmormar.opentasker.objectbuilders.NewNotaActivity;
+import com.jmormar.opentasker.objectmodifiers.ModifyNotasActivity;
+import com.jmormar.opentasker.util.DBHelper;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link NotasFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NotasFragment extends Fragment {
+public class NotasFragment extends Fragment implements NotaAdapter.OnNoteClickListener {
+    private static final String ARG_PARAM1 = "param1", ARG_PARAM2 = "param2";
+    private Context context;
+    private DBHelper helper;
+    private RecyclerView recyclerViewNotas;
+    private TextView tvNotasNoData;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public NotasFragment() {}
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public NotasFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Notas.
-     */
-    // TODO: Rename and change types and number of parameters
     public static NotasFragment newInstance(String param1, String param2) {
         NotasFragment fragment = new NotasFragment();
         Bundle args = new Bundle();
@@ -51,15 +50,60 @@ public class NotasFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            getArguments().getString(ARG_PARAM1);
+            getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notas, container, false);
+        this.context = requireContext();
+        this.helper = DBHelper.getInstance(this.context);
+        View rootView = inflater.inflate(R.layout.fragment_notas, container, false);
+
+        this.recyclerViewNotas = rootView.findViewById(R.id.rv_notas_notas);
+        this.tvNotasNoData = rootView.findViewById(R.id.tv_notas_nodata);
+
+        recyclerViewNotas.setLayoutManager(new GridLayoutManager(this.context, 2));
+
+        FloatingActionButton btAddNota = rootView.findViewById(R.id.fab_nueva_nota);
+        btAddNota.setOnClickListener(v -> irNuevaNota());
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        cargarNotas();
+    }
+
+    private void cargarNotas() {
+        if(helper == null) helper = DBHelper.getInstance(this.context);
+        List<Nota> notas = helper.getNotas();
+
+        if(!notas.isEmpty()){
+            this.tvNotasNoData.setVisibility(View.GONE);
+        } else{
+            this.tvNotasNoData.setVisibility(View.VISIBLE);
+        }
+
+        NotaAdapter notaAdapter = new NotaAdapter(this.context, notas);
+        notaAdapter.setOnNoteClickListener(this);
+
+        recyclerViewNotas.setAdapter(notaAdapter);
+    }
+
+    @Override
+    public void onNoteClick(int position) {
+        Intent myIntent = new Intent(this.context, ModifyNotasActivity.class);
+        myIntent.putExtra("position", position);
+        startActivity(myIntent);
+    }
+
+    public void irNuevaNota() {
+        Intent myIntent = new Intent(this.context, NewNotaActivity.class);
+        startActivity(myIntent);
     }
 }
