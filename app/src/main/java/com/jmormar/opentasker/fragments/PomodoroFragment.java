e5aa137588f01107c10091347e3581720110be1b
jmormar2412 <117973709+jmormar2412@users.noreply.github.com>
2024-05-19 17:45:51 +0200

    Finalización

--- app/src/main/java/com/jmormar/opentasker/fragments/PomodoroFragment.java
+++ app/src/main/java/com/jmormar/opentasker/fragments/PomodoroFragment.java
@@ -1,43 +1,47 @@
 package com.jmormar.opentasker.fragments;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.jmormar.opentasker.R;
+import com.jmormar.opentasker.adapters.PomodoroAdapter;
+import com.jmormar.opentasker.models.Pomodoro;
+import com.jmormar.opentasker.activities.TiemposActivity;
+import com.jmormar.opentasker.util.DBHelper;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link PomodoroFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PomodoroFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class PomodoroFragment extends Fragment implements PomodoroAdapter.OnPomodoroClickListener {
+    private static final String ARG_PARAM1 = "param1",ARG_PARAM2 = "param2";
+    private Context context;
+    TextView tvPomodorosNoData;
+    private RecyclerView recyclerViewPomodoros;
+    private DBHelper helper;
+    private AlertDialog.Builder builder;
 
     public PomodoroFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Pomodoro.
-     */
-    // TODO: Rename and change types and number of parameters
     public static PomodoroFragment newInstance(String param1, String param2) {
         PomodoroFragment fragment = new PomodoroFragment();
         Bundle args = new Bundle();
@@ -51,15 +55,93 @@ public class PomodoroFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            getArguments().getString(ARG_PARAM1);
+            getArguments().getString(ARG_PARAM2);
         }
     }
 
+    //TODO: Set slide to remove
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pomodoro, container, false);
+        this.context = requireContext();
+        this.helper = DBHelper.getInstance(this.context);
+        View rootView = inflater.inflate(R.layout.fragment_pomodoro, container, false);
+
+        this.recyclerViewPomodoros = rootView.findViewById(R.id.rv_pomodoros);
+        this.tvPomodorosNoData = rootView.findViewById(R.id.tv_pomodoros_nodata);
+
+        recyclerViewPomodoros.setLayoutManager(new LinearLayoutManager(this.context));
+
+        FloatingActionButton btAddPomodoro = rootView.findViewById(R.id.fab_nuevo_pomodoro);
+        btAddPomodoro.setOnClickListener(v -> this.builder.show());
+
+        createBuilder();
+        cargarPomodoros();
+
+        return rootView;
+    }
+
+    private void createBuilder() {
+        this.builder = new AlertDialog.Builder(this.context);
+        builder.setTitle("Nombre del pomodoro");
+
+        final EditText input = new EditText(this.context);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", (dialog, which) ->{
+            if(input.getText().toString().isEmpty()){
+                input.setError("Campo requerido");
+                return;
+            }
+            newPomodoro(input.getText().toString());
+        });
+        builder.setNegativeButton("Cancelar", (dialog, which) ->{
+            dialog.cancel();
+            createBuilder();
+        } );
+    }
+
+    private void newPomodoro(String name) {
+        Pomodoro pomodoro = new Pomodoro();
+        pomodoro.setNombre(name);
+        assert helper.insertarPomodoro(pomodoro) : "Error al insertar pomodoro";
+        pomodoro = helper.getPomodoros().get(helper.getPomodoros().size() - 1);
+
+        Intent myIntent = new Intent(this.context, TiemposActivity.class);
+        myIntent.putExtra("idPomodoro", pomodoro.getIdPomodoro());
+        startActivity(myIntent);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        cargarPomodoros();
+    }
+
+    private void cargarPomodoros() {
+        if(helper == null) helper = DBHelper.getInstance(this.context);
+        List<Pomodoro> pomodoros = helper.getPomodoros();
+
+        if(!pomodoros.isEmpty()){
+            this.tvPomodorosNoData.setVisibility(View.GONE);
+        } else{
+            this.tvPomodorosNoData.setVisibility(View.VISIBLE);
+        }
+
+        PomodoroAdapter pomodoroAdapter = new PomodoroAdapter(pomodoros);
+        pomodoroAdapter.setOnPomodoroClickListener(this);
+
+        recyclerViewPomodoros.setAdapter(pomodoroAdapter);
+    }
+
+    @Override
+    public void onPomodoroClick(int position) {
+        int idPomodoro = helper.getPomodoros().get(position).getIdPomodoro();
+        Intent myIntent = new Intent(this.context, TiemposActivity.class);
+        myIntent.putExtra("idPomodoro", idPomodoro);
+        startActivity(myIntent);
     }
 }
