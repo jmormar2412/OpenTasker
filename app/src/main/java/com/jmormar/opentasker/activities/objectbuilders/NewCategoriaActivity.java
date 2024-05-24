9bf314a032dec5abe49e041fa5d142029e517d06
jmormar2412 <117973709+jmormar2412@users.noreply.github.com>
2024-05-25 01:00:12 +0200

    Implementación

--- app/src/main/java/com/jmormar/opentasker/activities/objectbuilders/NewCategoriaActivity.java
+++ app/src/main/java/com/jmormar/opentasker/activities/objectbuilders/NewCategoriaActivity.java
@@ -1,6 +1,8 @@
 package com.jmormar.opentasker.activities.objectbuilders;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +11,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.jmormar.opentasker.R;
+import com.jmormar.opentasker.models.Categoria;
+import com.jmormar.opentasker.util.DBHelper;
+
+import top.defaults.colorpicker.ColorObserver;
+import top.defaults.colorpicker.ColorWheelView;
 
 public class NewCategoriaActivity extends AppCompatActivity {
+    private EditText etNombre;
+    private int color;
+    private DBHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +32,29 @@ public class NewCategoriaActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        this.etNombre = findViewById(R.id.et_nombre_categoria);
+        this.helper = DBHelper.getInstance(this);
+
+        ColorWheelView colorWheelView = findViewById(R.id.color_wheel);
+        colorWheelView.subscribe((color, fromUser, shouldPropagate) -> this.color = color);
+
+        findViewById(R.id.bt_guardar).setOnClickListener((v) -> guardar());
+    }
+
+    private void guardar() {
+        String nombre = this.etNombre.getText().toString();
+        if (nombre.isEmpty()) {
+            this.etNombre.setError("El nombre es obligatorio");
+            return;
+        }
+
+        Categoria categoria = new Categoria();
+        categoria.setNombre(nombre);
+        categoria.setColor(this.color);
+        categoria.setIdAgenda(helper.getAgenda().getIdAgenda());
+
+        assert helper.insertarCategoria(categoria) : "Error al insertar la categoria";
+        finish();
     }
 }
